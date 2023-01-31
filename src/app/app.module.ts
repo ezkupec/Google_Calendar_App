c80002f65617dc12b078e925f766e840e0e898e4
ezkupec <ezk00001@mix.wvu.edu>
2023-01-31 16:03:51 -0500

    Top Nav Bars
    Added top two nav bars to webapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './Navigation/top-nav-bar/top-nav-bar.component';
+import { SubNavBarComponent } from './Navigation/sub-nav-bar/sub-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    SubNavBarComponent
   ],
   imports: [
     BrowserModule,
